a3f9f1a86e930d160c1636a5f6c064c9ac025aaf
d0rshBooken <andvr916@student.liu.se>
2019-04-16 14:33:57 +0200

    Patch v1.9.1

--- src/android/ReportFullyDrawn.java
+++ src/android/ReportFullyDrawn.java
@@ -29,11 +29,6 @@ import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
@@ -186,3 +181,5 @@ public class ReportFullyDrawn extends CordovaPlugin {
                 PackageManager.PERMISSION_GRANTED;
     }
 }
+
+//cordova plugin add cordova-plugin-android-support-v4
